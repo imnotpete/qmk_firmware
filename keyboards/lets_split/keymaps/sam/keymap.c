5b2d71ff7a6c5d0528879f52551b29b90ef3bc86
Sam Jones <sam@imnotpete.com>
2017-11-05 22:21:59 -0500

    shortened tap dance duration. switched tab key back to just tab

--- keyboards/lets_split/keymaps/sam/keymap.c
+++ keyboards/lets_split/keymaps/sam/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * `-----------------------------------------------------------------------------------'
 */
 [_QWERTY] = KEYMAP( \
-		TD_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+		KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
 		KC_ESC,  KC_A,   KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
 		KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
 		KC_LCTL, NUMPAD, KC_LGUI, KC_LALT, LW_SPC, FN_SPC, FN_SPC, RS_SPC, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
@@ -137,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 [_FN] = KEYMAP( \
 		M_TBPRV, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_DEL, \
-		M_TBNXT, _______, _______, _______, FIND,    KC_F3,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+		M_TBNXT, _______, _______, _______, FIND,    KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
 		_______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, \
 		M_CAD,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
@@ -243,14 +243,6 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 		return MACRO_NONE;
 };
 
-void dance_tab_esc(qk_tap_dance_state_t *state, void *user_data) {
-		if (state->count == 1) {
-				register_code(KC_TAB);
-		} else {
-				register_code(KC_ESC);
-		}
-}
-
 qk_tap_dance_action_t tap_dance_actions[] = {
-		[TD_TAB_ESC] = ACTION_TAP_DANCE_FN(dance_tab_esc)
+		[TD_TAB_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC)
 };
