fd64172faddbecb8a020674a2ec4ffd380b469d2
Samuel L. Jones <1123183548@mil>
2017-11-14 15:38:20 -0500

    added ss_lsft and updated keymaps

--- keyboards/nyquist/keymaps/sam/keymap.c
+++ keyboards/nyquist/keymaps/sam/keymap.c
@@ -54,20 +54,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |Adjust| GUI  | Alt  |Lower |FnSpce|FnSpce|Raise | Home | PgDn | PgUp | End  |
+ * | Ctrl |NumPad| GUI  | Alt  |Lower |FnSpce|FnSpce|Raise | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   KC_LCTL, NUMPAD,  KC_LGUI, KC_LALT, LW_SPC,  FN_SPC,  FN_SPC,  RS_SPC,  KC_HOME, KC_PGDN, KC_PGUP, KC_END \
@@ -150,35 +150,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * `-----------------------------------------------------------------------------------'
 */
 [_FN] = KEYMAP( \
-		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-		M_TBPRV, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_DEL, \
-		M_TBNXT, _______, _______, _______, FIND,    KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-		_______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, \
-		M_CAD,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+	_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+	M_TBPRV, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_DEL, \
+	M_TBNXT, _______, _______, _______, FIND,    KC_F3,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+	_______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, \
+	M_CAD,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* NumPad
 * ,-----------------------------------------------------------------------------------.
-* |      |      |      |      |      |      |      |      |      |      |      |      |
-* |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
-* |------+------+------+------+------+------+------+------+------+------+------+------|
-* |      |      |      |      |      |      |  7   |  8   |  9   |  *   |      |      |
+* |      |      |      |      |      |      |      |  7   |  8   |  9   |  *   |      |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* |      |      |      |      |      |      |  4   |  5   |  6   |  -   |      |      |
+* |      |      |      |      |      |      |      |  4   |  5   |  6   |  -   |      |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |      |      |      |      |      |      |  1   |  2   |  3   |  +   |      |      |
+* |      |      |      |      |      |      |      |  1   |  2   |  3   |  +   |      |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* |      |      |      |      |      |      |  0   |      |  .   |  /   |      |      |
+* |      |      |      |      |      |      |      |  0   |      |  .   |  /   |      |
+* |------+------+------+------+------+------+------+------+------+------+------+------|
+* |      |      |      |      |      |             |      |      |      |      |      |
 * `-----------------------------------------------------------------------------------'
 */
 [_NUMPAD] = KEYMAP( \
-		_______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_ASTR, _______, \
-		_______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_MINS, _______, \
-		_______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_PLUS, _______, \
-		_______, _______, _______, _______, _______, _______, _______, KC_0,    KC_ENT,  KC_DOT,  KC_SLSH, _______, \
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
+	_______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_ASTR, _______, \
+	_______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    KC_MINS, _______, \
+	_______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_PLUS, _______, \
+	_______, _______, _______, _______, _______, _______, _______, KC_0,    KC_ENT,  KC_DOT,  KC_SLSH, _______, \
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
 )
-
 
 };
 
@@ -188,82 +186,87 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-		switch (keycode) {
-				case QWERTY:
-						if (record->event.pressed) {
-								persistent_default_layer_set(1UL<<_QWERTY);
-						}
-						return false;
-						break;
-				case LOWER:
-						if (record->event.pressed) {
-								layer_on(_LOWER);
-								update_tri_layer(_LOWER, _RAISE, _ADJUST);
-						} else {
-								layer_off(_LOWER);
-								update_tri_layer(_LOWER, _RAISE, _ADJUST);
-						}
-						return false;
-						break;
-				case RAISE:
-						if (record->event.pressed) {
-								layer_on(_RAISE);
-								update_tri_layer(_LOWER, _RAISE, _ADJUST);
-						} else {
-								layer_off(_RAISE);
-								update_tri_layer(_LOWER, _RAISE, _ADJUST);
-						}
-						return false;
-						break;
-				case ADJUST:
-						if (record->event.pressed) {
-								layer_on(_ADJUST);
-						} else {
-								layer_off(_ADJUST);
-						}
-						return false;
-						break;
-				case FN:
-						if (record->event.pressed) {
-								layer_on(_FN);
-						} else {
-								layer_off(_FN);
-						}
-						return false;
-						break;
-				case NUMPAD:
-						if (record->event.pressed) {
-								layer_on(_NUMPAD);
-						} else {
-								layer_off(_NUMPAD);
-						}
-						return false;
-						break;
-		}
-		return true;
+	switch (keycode) {
+		case QWERTY:
+			if (record->event.pressed) {
+				persistent_default_layer_set(1UL<<_QWERTY);
+			}
+			return false;
+			break;
+		case LOWER:
+			if (record->event.pressed) {
+				layer_on(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+		case RAISE:
+			if (record->event.pressed) {
+				layer_on(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+		case ADJUST:
+			if (record->event.pressed) {
+				layer_on(_ADJUST);
+			} else {
+				layer_off(_ADJUST);
+			}
+			return false;
+			break;
+		case FN:
+			if (record->event.pressed) {
+				layer_on(_FN);
+			} else {
+				layer_off(_FN);
+			}
+			return false;
+			break;
+		case NUMPAD:
+			if (record->event.pressed) {
+				layer_on(_NUMPAD);
+			} else {
+				layer_off(_NUMPAD);
+			}
+			return false;
+			break;
+	}
+	return true;
 }
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-		switch(id) {
-				case TBNXT:
-						if (record->event.pressed) {
-								return MACRO( D(LCTL), T(PGDN), U(LCTL), END  ); 
-						}
-						break;
-				case TBPRV: 
-						if (record->event.pressed) {
-								return MACRO( D(LCTL), T(PGUP), U(LCTL), END  ); 
-						}
-						break;
-				case CAD: 
-						if (record->event.pressed) {
-								return MACRO( D(LCTL), D(LALT), T(DEL), U(LALT), U(LCTL), END  ); 
-						}
-						break;
-		}
-		return MACRO_NONE;
+	switch(id) {
+		case TBNXT:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(PGDN), U(LCTL), END  ); 
+			}
+			break;
+		case TBPRV: 
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(PGUP), U(LCTL), END  ); 
+			}
+			break;
+		case CAD: 
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), D(LALT), T(DEL), U(LALT), U(LCTL), END  ); 
+			}
+			break;
+		case CTRL_W: 
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(X), U(LCTL), END  ); 
+			}
+			break;
+	}
+	return MACRO_NONE;
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-		[TD_TAB_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC)
+	[TD_TAB_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC)
 };
--- keyboards/sweet16/keymaps/sam/keymap.c
+++ keyboards/sweet16/keymaps/sam/keymap.c
@@ -5,19 +5,19 @@
 #define LAYOUT_NUMPAD 1
 
 enum custom_keycodes {
-		UP_URL = SAFE_RANGE,
-		CAD, // Ctrl+Alt+Del
-		CSE, // Ctrl+Shift+Esc
-		COMMENT, // HTML open comment
-		SCRSHT // Take screenshot, paste into Paint
+	UP_URL = SAFE_RANGE,
+	CAD, // Ctrl+Alt+Del
+	CSE, // Ctrl+Shift+Esc
+	COMMENT, // HTML open comment
+	SCRSHT // Take screenshot, paste into Paint
 };
 
 enum {
-		TD_EMAIL_NOTEPAD = 0, // single tap open notepad; double type email header
-		TD_DEL_CAD, // single CAD, double Del
-		TD_ESC_F12, // single Esc, double F12
-		TD_PLAY_LAYER, // single play/pause, triple layer
-		TD_ENTER_LAYER // single Enter, triple layer
+	TD_EMAIL_NOTEPAD = 0, // single tap open notepad; double type email header
+	TD_DEL_CAD, // single CAD, double Del
+	TD_ESC_F12, // single Esc, double F12
+	TD_PLAY_LAYER, // single play/pause, triple layer
+	TD_ENTER_LAYER // single Enter, triple layer
 };
 
 #define TD_EMAIL TD(TD_EMAIL_NOTEPAD)
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------'
  */
 [LAYOUT_MACROS] = KEYMAP(
-		TD_ESC,  COMMENT, TD_EMAIL, SCRSHT,
-		KC_F5,   KC_F6,   KC_F7,    KC_F8,
-		KC_MUTE, KC_VOLD, KC_VOLU,  CSE,
-		KC_MPRV, PLAYER,  KC_MNXT,  TD_DEL 
+	TD_ESC,  COMMENT, TD_EMAIL, SCRSHT,
+	KC_F5,   KC_F6,   KC_F7,    KC_F8,
+	KC_MUTE, KC_VOLD, KC_VOLU,  CSE,
+	KC_MPRV, PLAYER,  KC_MNXT,  TD_DEL 
 ), 
 
 /* Numpad
@@ -58,84 +58,85 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------'
  */
 [LAYOUT_NUMPAD] = KEYMAP(
-		KC_7, KC_8,   KC_9,   KC_ASTR,
-		KC_4, KC_5,   KC_6,   KC_MINS,
-		KC_1, KC_2,   KC_3,   KC_PLUS,
-		KC_0, LAYTER, KC_DOT, KC_SLSH
+	KC_7, KC_8,   KC_9,   KC_ASTR,
+	KC_4, KC_5,   KC_6,   KC_MINS,
+	KC_1, KC_2,   KC_3,   KC_PLUS,
+	KC_0, LAYTER, KC_DOT, KC_SLSH
 )
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
-		eeconfig_update_default_layer(default_layer);
-		default_layer_set(default_layer);
+	eeconfig_update_default_layer(default_layer);
+	default_layer_set(default_layer);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-		switch (keycode) {
-				case CSE: 
-						if (record->event.pressed) {
-								SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LSHIFT) SS_TAP(X_ESCAPE) SS_UP(X_LSHIFT) SS_UP(X_LCTRL));
-								return false;
-						}
-						break;
-				case COMMENT: 
-						if (record->event.pressed) {
-								SEND_STRING("<!--");
-								return false;
-						}
-						break;
-				case SCRSHT: 
-						if (record->event.pressed) {
-								SEND_STRING(SS_TAP(X_PSCREEN) SS_LGUI("r"));
-								_delay_ms(750);
-								SEND_STRING("mspaint" SS_TAP(X_ENTER));
-								_delay_ms(750);
-								SEND_STRING(SS_LCTRL("v"));
-								return false;
-						}
-						break;
-		}
-		return true;
+	switch (keycode) {
+		case CSE: 
+			if (record->event.pressed) {
+				//SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LSHIFT) SS_TAP(X_ESCAPE) SS_UP(X_LSHIFT) SS_UP(X_LCTRL));
+				SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_ESC)))); 
+				return false;
+			}
+			break;
+		case COMMENT: 
+			if (record->event.pressed) {
+				SEND_STRING("<!--");
+				return false;
+			}
+			break;
+		case SCRSHT: 
+			if (record->event.pressed) {
+				SEND_STRING(SS_TAP(X_PSCREEN) SS_LGUI("r"));
+				_delay_ms(750);
+				SEND_STRING("mspaint" SS_TAP(X_ENTER));
+				_delay_ms(750);
+				SEND_STRING(SS_LCTRL("v"));
+				return false;
+			}
+			break;
+	}
+	return true;
 };
 
 void dance_email_notepad(qk_tap_dance_state_t *state, void *user_data) {
-		if (state->count == 1) {
-				SEND_STRING(SS_LGUI("r"));
-				_delay_ms(750);
-				SEND_STRING("notepad" SS_TAP(X_ENTER));
-		} else {
-				send_string(my_str);
-		}
+	if (state->count == 1) {
+		SEND_STRING(SS_LGUI("r"));
+		_delay_ms(750);
+		SEND_STRING("notepad" SS_TAP(X_ENTER));
+	} else {
+		send_string(my_str);
+	}
 }
 
 void dance_delete_cad(qk_tap_dance_state_t *state, void *user_data) {
-		if (state->count == 1) {
-				SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_DELETE))));
-		} else {
-				SEND_STRING(SS_TAP(X_DELETE));
-		}
+	if (state->count == 1) {
+		SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_DELETE))));
+	} else {
+		SEND_STRING(SS_TAP(X_DELETE));
+	}
 }
 
 void dance_player(qk_tap_dance_state_t *state, void *user_data) {
-		if (state->count <= 2) {
-				register_code(KC_MPLY);
-		} else {
-				persistent_default_layer_set(1UL<<LAYOUT_NUMPAD);
-		}
+	if (state->count <= 2) {
+		register_code(KC_MPLY);
+	} else {
+		persistent_default_layer_set(1UL<<LAYOUT_NUMPAD);
+	}
 }
 
 void dance_layter(qk_tap_dance_state_t *state, void *user_data) {
-		if (state->count <= 2) {
-				SEND_STRING(SS_TAP(X_ENTER));
-		} else {
-				persistent_default_layer_set(1UL<<LAYOUT_MACROS);
-		}
+	if (state->count <= 2) {
+		SEND_STRING(SS_TAP(X_ENTER));
+	} else {
+		persistent_default_layer_set(1UL<<LAYOUT_MACROS);
+	}
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-		[TD_EMAIL_NOTEPAD] = ACTION_TAP_DANCE_FN(dance_email_notepad),
-		[TD_DEL_CAD] = ACTION_TAP_DANCE_FN(dance_delete_cad),
-		[TD_ESC_F12] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_F12),
-		[TD_PLAY_LAYER] = ACTION_TAP_DANCE_FN(dance_player),
-		[TD_ENTER_LAYER] = ACTION_TAP_DANCE_FN(dance_layter)
+	[TD_EMAIL_NOTEPAD] = ACTION_TAP_DANCE_FN(dance_email_notepad),
+	[TD_DEL_CAD] = ACTION_TAP_DANCE_FN(dance_delete_cad),
+	[TD_ESC_F12] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_F12),
+	[TD_PLAY_LAYER] = ACTION_TAP_DANCE_FN(dance_player),
+	[TD_ENTER_LAYER] = ACTION_TAP_DANCE_FN(dance_layter)
 };
